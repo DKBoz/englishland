da16d35759f45ca58704988bb8ec9a1c13849c7c
DKBoz <theburakbozoglan@gmail.com>
2026-03-10 09:47:01 +0300

    fixed unit 8 listen bug

--- app/grade5/unit8/page.js
+++ app/grade5/unit8/page.js
@@ -1,25 +1,25 @@
 'use client'
-import ReadingListen from '@/components/activities/ReadingListen'
-import MiniStory from '@/components/activities/MiniStory'
-import DragSentence from '@/components/activities/DragSentence'
-import MatchGame from '@/components/activities/MatchGame'
-import Spelling from '@/components/activities/Spelling'
-import WordScramble from '@/components/activities/WordScramble'
 import UnitLayout from '@/components/UnitLayout'
 import VocabCards from '@/components/activities/VocabCards'
 import Quiz from '@/components/activities/Quiz'
-import { unitInfo, vocab, questions, scrambleWords, spellWords, matchPairs, dragSentences, story } from './data'
+import WordScramble from '@/components/activities/WordScramble'
+import Spelling from '@/components/activities/Spelling'
+import MatchGame from '@/components/activities/MatchGame'
+import DragSentence from '@/components/activities/DragSentence'
+import MiniStory from '@/components/activities/MiniStory'
+import ReadingListen from '@/components/activities/ReadingListen'
+import { unitInfo, vocab, questions, scrambleWords, spellWords, matchPairs, dragSentences, story, readingText } from './data'
 
 export default function Unit8Page() {
   const tabs = [
     { label: "📖 Words", component: <VocabCards words={vocab} /> },
+    { label: "🎧 Listen", component: <ReadingListen title={readingText.title} paragraphs={readingText.paragraphs} questions={readingText.questions} /> },
     { label: "✏️ Quiz", component: <Quiz questions={questions} /> },
     { label: "🔀 Scramble", component: <WordScramble words={scrambleWords} /> },
     { label: "🔤 Spelling", component: <Spelling words={spellWords} /> },
     { label: "🧩 Match", component: <MatchGame pairs={matchPairs} /> },
     { label: "🖱️ Sentences", component: <DragSentence sentences={dragSentences} /> },
     { label: "📖 Story", component: <MiniStory nodes={story.nodes} /> },
-    { label: "🎧 Listen", component: <ReadingListen /> },
   ]
 
   return (
